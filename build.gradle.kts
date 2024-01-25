b7974491b89d3361ebf16860affcbd17c75f9627
15754324647@163.com <15754324647@163.com>
2024-01-25 23:06:45 +0800

    [init]

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("java")
+    id("org.jetbrains.intellij") version "1.10.1"
+}
+
+group = "com.plugin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+//    mavenCentral()
+    maven { url = uri("https://maven.aliyun.com/repository/google") }
+    maven { url = uri("https://maven.aliyun.com/repository/jcenter") }
+    maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+}
+
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2022.3.3")
+    type.set("IC") // Target IDE Platform
+
+    plugins.set(listOf(/* Plugin Dependencies */))
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("221")
+        untilBuild.set("231.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
